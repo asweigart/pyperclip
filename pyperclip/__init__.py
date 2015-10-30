73adc7cc595874a59953199f1a6c505a3d72074b
Anton Yakutovich <drakulavich@gmail.com>
2015-10-30 12:18:15 +0300

    _pasteKlipper: remove last newline if exists

--- pyperclip/__init__.py
+++ pyperclip/__init__.py
@@ -181,10 +181,8 @@ def _pasteKlipper():
     # https://bugs.kde.org/show_bug.cgi?id=342874
 
     clipboardContents = stdout.decode('utf-8')
-    assert len(clipboardContents) > 0 # even if blank, Klipper will append a newline at the end
-    assert clipboardContents[-1] == '\n' # make sure that newline is there
-    if len(clipboardContents) and clipboardContents[-1] == '\n':
-        clipboardContents = clipboardContents[:-1]
+    if clipboardContents.endswith('\n'):
+      clipboardContents = clipboardContents[:-1]
     return clipboardContents
 
 def setFunctions(functionSet):
