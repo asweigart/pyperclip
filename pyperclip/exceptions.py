c74edaeb4c7590dad3fc38a91ba460dd7e50f1db
contrixed <contrixed@users.noreply.github.com>
2015-11-02 20:49:15 -0500

    Fix for Python 2.6.

--- pyperclip/exceptions.py
+++ pyperclip/exceptions.py
@@ -7,5 +7,5 @@ class PyperclipException(RuntimeError):
 
 class PyperclipWindowsException(PyperclipException):
     def __init__(self, message):
-        message += " ({0})".format(ctypes.WinError())
+        message += " ({})".format(ctypes.WinError())
         super(PyperclipWindowsException, self).__init__(message)
