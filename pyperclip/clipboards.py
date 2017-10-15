c290e3bae796f8a8145e82f8fb336d2dc59607d8
bmintz <bmintz@users.noreply.github.com>
2017-10-14 19:18:14 -0500

    clipboards: add Termux support
    pretty simple. it's just the same as the other commands
    future work should probably deduplicate all those `subprocess.Popen`s.

--- pyperclip/clipboards.py
+++ pyperclip/clipboards.py
@@ -24,6 +24,26 @@ def init_osx_clipboard():
     return copy_osx, paste_osx
 
 
+def init_termux_clipboard():
+    """return a copy and paste function for Termux,
+    a terminal application for Android.
+
+    more information: https://termux.com/"""
+
+    def copy_termux(text):
+        p = subprocess.Popen('termux-clipboard-set',
+                             stdin=subprocess.PIPE, close_fds=True)
+        p.communicate(input=text.encode('utf-8'))
+
+    def paste_termux():
+        p = subprocess.Popen('termux-clipboard-get',
+                             stdout=subprocess.PIPE, close_fds=True)
+        stdout, stderr = p.communicate()
+        return stdout.decode('utf-8')
+
+    return copy_termux, paste_termux
+
+
 def init_gtk_clipboard():
     import gtk
 
