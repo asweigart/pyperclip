e64dfb8db1843928ccd44b957b02ee670d8802a4
bmintz <bmintz@users.noreply.github.com>
2017-10-14 19:23:43 -0500

    bump minor version
    Termux support is a backwards-compatible new feature

--- pyperclip/__init__.py
+++ pyperclip/__init__.py
@@ -25,7 +25,7 @@ Otherwise on Linux, you will need the gtk or PyQt4 modules installed.
 gtk and PyQt4 modules are not available for Python 3,
 and this module does not work with PyGObject yet.
 """
-__version__ = '1.5.27'
+__version__ = '1.6'
 
 import platform
 import os
