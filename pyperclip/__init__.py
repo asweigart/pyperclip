fb483dd84c524d1f745956519814e9b163c8482b
Al Sweigart <asweigart@gmail.com>
2017-10-28 21:29:20 -0700

    Bump version number to 1.5.28

--- pyperclip/__init__.py
+++ pyperclip/__init__.py
@@ -23,7 +23,7 @@ Otherwise on Linux, you will need the gtk or PyQt4 modules installed.
 gtk and PyQt4 modules are not available for Python 3,
 and this module does not work with PyGObject yet.
 """
-__version__ = '1.5.27'
+__version__ = '1.5.28'
 
 import platform
 import os
