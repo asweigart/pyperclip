eb56a82cc57daa9ea037555d18130372e5eede47
J. Junquera <javier.junquera.sanchez@gmail.com>
2018-10-08 23:02:33 +0200

    Its better as a class

--- src/pyperclip/monitor/__init__.py
+++ src/pyperclip/monitor/__init__.py
@@ -1,14 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Pyperclip Monitor
+
+Utility for monitoring the clipboard based on Pyperclip
+
+Usage:
+    from pyperclip.monitor import Monitor
+
+    for entry in Monitor():
+        print("Added to clipboard: {}".format(entry))
+
+"""
+
 import pyperclip
 import time
 
 
-def monitor(warm_start=False):
+class Monitor(object):
 
-    last = pyperclip.paste() if not warm_start else None
+    '''
+    '''
+    def __init__(self, warm_start=False):
+        self.warm_start = warm_start
 
-    while 1:
-        if pyperclip.paste() == last:
-            time.sleep(0.1)
-        else:
-            last = pyperclip.paste()
-            yield last
+    def __iter__(self):
+
+        last = pyperclip.paste() if not self.warm_start else None
+
+        while True:
+            if pyperclip.paste() == last:
+                time.sleep(0.1)
+            else:
+                last = pyperclip.paste()
+                yield last
