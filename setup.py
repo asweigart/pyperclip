62b6e365c323664a9b2a47a95538092fef990a2d
Hugo <hugovk@users.noreply.github.com>
2019-08-07 17:27:44 +0300

    Drop support for EOL Python 3.4

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     package_dir={'': 'src'},
     test_suite='tests',
     keywords="clipboard copy paste clip xsel xclip",
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Win32 (MS Windows)',
@@ -36,7 +37,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
