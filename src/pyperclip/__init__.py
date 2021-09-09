0f6afa95b27987676cd13a6bd673154764d52cf5
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-09-09 10:39:48 +0300

    Drop support for EOL Python 2

--- src/pyperclip/__init__.py
+++ src/pyperclip/__init__.py
@@ -53,7 +53,6 @@ import ctypes
 import os
 import platform
 import subprocess
-import sys
 import time
 import warnings
 
@@ -68,10 +67,6 @@ HAS_DISPLAY = os.getenv("DISPLAY", False)
 EXCEPT_MSG = """
     Pyperclip could not find a copy/paste mechanism for your system.
     For more information, please visit https://pyperclip.readthedocs.io/en/latest/index.html#not-implemented-error """
-
-PY2 = sys.version_info[0] == 2
-
-STR_OR_UNICODE = unicode if PY2 else str # For paste(): Python 3 uses str, Python 2 uses unicode.
 
 ENCODING = 'utf-8'
 
@@ -102,14 +97,11 @@ class PyperclipWindowsException(PyperclipException):
 class PyperclipTimeoutException(PyperclipException):
     pass
 
+
 def _stringifyText(text):
-    if PY2:
-        acceptedTypes = (unicode, str, int, float, bool)
-    else:
-        acceptedTypes = (str, int, float, bool)
-    if not isinstance(text, acceptedTypes):
+    if not isinstance(text, (str, int, float, bool)):
         raise PyperclipException('only str, int, float, and bool values can be copied to the clipboard, not %s' % (text.__class__.__name__))
-    return STR_OR_UNICODE(text)
+    return str(text)
 
 
 def init_osx_pbcopy_clipboard():
@@ -194,7 +186,7 @@ def init_qt_clipboard():
 
     def paste_qt():
         cb = app.clipboard()
-        return STR_OR_UNICODE(cb.text())
+        return str(cb.text())
 
     return copy_qt, paste_qt
 
@@ -335,12 +327,8 @@ def init_no_clipboard():
         def __call__(self, *args, **kwargs):
             raise PyperclipException(EXCEPT_MSG)
 
-        if PY2:
-            def __nonzero__(self):
-                return False
-        else:
-            def __bool__(self):
-                return False
+        def __bool__(self):
+            return False
 
     return ClipboardUnavailable(), ClipboardUnavailable()
 
